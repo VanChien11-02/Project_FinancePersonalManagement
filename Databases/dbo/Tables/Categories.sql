788cff7de26241d3b7c4fff5505432f322d8e709
Tri Thai <NguyenTT162.4@gmail.com>
2026-03-30 15:03:08 +0700

    added tables

---
+++ Databases/dbo/Tables/Categories.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Categories] (
+    [CategoryID]   VARCHAR (7)    NOT NULL,
+    [UserID]       VARCHAR (7)    NOT NULL,
+    [CategoryName] NVARCHAR (100) NOT NULL,
+    [CategoryType] NVARCHAR (20)  NOT NULL,
+    [Note]         NVARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([CategoryID] ASC),
+    FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([UserID])
+);
+
---
+++ Databases/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserID]       VARCHAR (7)   NOT NULL,
+    [Username]     NVARCHAR (50) NOT NULL,
+    [PasswordHash] VARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC),
+    UNIQUE NONCLUSTERED ([Username] ASC)
+);
+
